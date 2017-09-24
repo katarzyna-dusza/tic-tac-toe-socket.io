71351a470d1bcc5e9b443de92136b8863d91e7f4
Katarzyna Dusza <katarzyna.jolanta.dusza@gmail.com>
2017-09-24 16:55:33 +0200

    provide const variable for max number of players

--- server.js
+++ server.js
@@ -5,34 +5,38 @@ const io = require("socket.io")(http);
 const _ = require("underscore")._;
 const gameService = require("./server-service/game-service.module.js");
 
+const MAX_USERS_NUMBER = 2;
 let users = [];
 let left = "";
 let isTurn = "";
 let randomSign = "";
 let board = [];
+let signs = {};
 
 app.use(express.static(__dirname + "/"));
 app.use('/js', express.static('./node_modules/snapsvg/dist'));
 
 function createPlayers(socket) {
     if (0 === users.length) {
-        randomSign = gameService.assignSignToFirstUser().randomSign;
+        signs = gameService.assignSignToFirstUser();
+        randomSign = signs.randomSign;
         isTurn = randomSign;
-        left = gameService.assignSignToFirstUser().left;
+        left = signs.left;
 
         users.push(gameService.addNewUser(socket, randomSign));
         return socket.emit("turn", isTurn);
     }
 
+
     return users.push(gameService.addNewUser(socket, left));
 }
 
 function createNewUsers(socket) {
-    if (2 > users.length) {
+    if (MAX_USERS_NUMBER > users.length) {
         return createPlayers(socket);
     }
 
-    socket.disconnect();
+    return socket.disconnect();
 }
 
 io.on("connection", (socket) => {
